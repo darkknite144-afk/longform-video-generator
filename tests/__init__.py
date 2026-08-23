d6d62300230325450660d6a8b896a4afe006c553
darkknite144-afk <darkknite144@gmail.com>
2026-08-23 07:32:20 +0530

    Add tests/__init__.py

---
+++ tests/__init__.py
@@ -0,0 +1,6 @@
+"""
+Comprehensive test suite for the longform video generator.
+
+This file aggregates all test modules. Running `pytest` from the project
+root will discover and run all tests automatically.
+"""
